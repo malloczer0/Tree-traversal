eb53ba72c93ff4c9a42b879c1b7ce592d51dbf66
Leonid Filippov <malloczero@icloud.com>
2020-09-16 00:50:59 +0300

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,37 @@
+package breathFirstTreeTraversal
+
+import java.util.*
+import kotlin.contracts.contract
+
+data class Node<T>(val data: T, val nodes: List<Node<T>> = emptyList())
+
+fun traversalBFS(rootNode: Node<Int>) {
+    var queue: Queue<Node<Int>> = LinkedList()
+    queue.add(rootNode)
+    while (queue.isNotEmpty()) {
+        print( "${queue.peek().data} ")
+        queue.remove().nodes.forEach { queue.add(it) }
+    }
+}
+
+fun traversalDFS(rootNode: Node<Int>) {
+    var stack: Stack<Node<Int>> = Stack()
+    stack.add(rootNode)
+    while (stack.isNotEmpty()) {
+        print("${stack.peek().data} ")
+        stack.pop().nodes.forEach { stack.add(it) }
+    }
+}
+
+fun main() {
+    val tree: Node<Int>
+            = Node<Int>(14, listOf(Node<Int>(88, emptyList()),
+            Node<Int>(13, listOf(Node<Int>(37, emptyList()),
+            Node<Int>(2, emptyList()),
+            Node<Int>(28, emptyList())))))
+
+    println("Как же я люблю BTS вот они слева направо:")
+    traversalBFS(tree)
+    println()
+    traversalDFS(tree)
+}
